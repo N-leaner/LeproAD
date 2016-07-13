6ff80831118c11cc7bdcf5f6a31d2de81ae59a24
N-leaner <yahoo@pizd.ec>
2016-07-13 13:08:08 +0300

    Add classes for db

--- app.rb
+++ app.rb
@@ -2,6 +2,18 @@
 require 'rubygems'
 require 'sinatra'
 require 'sinatra/reloader'
+require 'sinatra/activerecord'
+
+set :database, "sqlite3:lepro.db"
+
+class User < ActiveRecord::Base
+end	
+
+class Post < ActiveRecord::Base
+end	
+
+class Comment < ActiveRecord::Base
+end	
 
 get '/' do
 	erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>"			
