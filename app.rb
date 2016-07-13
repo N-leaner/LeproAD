2de354ab47faa9b8d3134c09ac6d5109570640d8
N-leaner <yahoo@pizd.ec>
2016-07-13 14:00:45 +0300

    Add validations

--- app.rb
+++ app.rb
@@ -8,14 +8,18 @@ set :database, "sqlite3:lepro.db"
 
 class User < ActiveRecord::Base
 	has_many :posts
+	validates :name, presence: true, length: { in: 1..50 }
+	validates :password, presence: true, length: { in: 3..20 }
 end	
 
 class Post < ActiveRecord::Base
 	has_many :comments
+	validates :post, presence: true, length: { in: 1..500 }
 end	
 
 class Comment < ActiveRecord::Base
 	belongs_to :user
+	validates :comment, presence: true, length: { in: 1..500 }
 end	
 
 get '/' do
