ae204221bc14a02579740e25eec82a089edfb0f3
N-leaner <yahoo@pizd.ec>
2016-07-14 08:50:21 +0300

    Add register page

--- app.rb
+++ app.rb
@@ -29,3 +29,7 @@ end
 get '/' do
 	erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>"			
 end
+
+get '/register' do
+  erb :register
+end
